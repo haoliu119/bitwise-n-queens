9d4afc2fbbc0df7617eb95fb22551edc2246cfea
Alex Notov <anotov@gmail.com>
2012-12-14 14:58:50 -0800

    Use backbone, ChessboardView vs Chessboard

---
+++ spec/ChessboardViewSpec.js
@@ -0,0 +1,11 @@
+describe("ChessboardView", function() {
+  var view, collection;
+
+  beforeEach(function() {
+    view = new ChessboardView();
+  });
+
+  it("should exist", function() {
+    expect(view).toBeTruthy();
+  });
+});

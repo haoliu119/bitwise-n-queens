0ca909af80073378000bff4ebe8a0ca148c39467
Catalyst Students <accounts@catalystclass.com>
2012-12-16 15:23:39 -0800

    clearer way forward

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
 $(function(){
-  var chessboard = new ChessboardView();
-  $("body").append(chessboard.render());
+  window.chessboardView = new ChessboardView();
+  $("body").append(chessboardView.render());
+
+  // note: you can switch this out for solveNQueens when you're ready!
+  solveNRooks(8);
 });

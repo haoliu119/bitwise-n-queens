b80ea6a82b8680458629a25089cb1d45a463db2b
Marcus Phillips <github.com@marcusphillips.com>
2013-04-11 01:57:58 -0700

    test language clarified

--- spec/BoardSpec.js
+++ spec/BoardSpec.js
@@ -4,94 +4,58 @@ describe("Board", function() {
     return word[0].toUpperCase() + word.slice(1);
   };
 
-  var checkAllConflictTypes = function(matrix, conflictExpectations){
+  var verifyConflictTypes = function(expectedConflicts, matrix){
     var board = new Board(matrix);
     _.map('row col rooks majorDiagonal minorDiagonal queens'.split(' '), function(conflictType){
       var conflictDetected = board['hasAny' + capitalize(conflictType) + 'Conflicts']();
-      var conflictExpected = conflictExpectations[conflictType];
+      var conflictExpected = _(expectedConflicts).contains(conflictType);
       expect(conflictDetected).toEqual(conflictExpected);
     });
   };
 
   it("should find non conflicts", function() {
-    checkAllConflictTypes([
+    verifyConflictTypes([], [
       [0, 0, 0, 0],
       [0, 0, 0, 0],
       [0, 0, 0, 0],
       [0, 0, 0, 0]
-    ], {
-      row: false,
-      col: false,
-      majorDiagonal: false,
-      minorDiagonal: false,
-      rooks: false,
-      queens: false
-    });
+    ]);
   });
 
   it("should find row conflicts", function() {
-    checkAllConflictTypes([
+    verifyConflictTypes(['row', 'rooks', 'queens'], [
       [0, 0, 0, 0],
       [1, 1, 0, 0],
       [0, 0, 0, 0],
       [0, 0, 0, 0]
-    ], {
-      row: true,
-      col: false,
-      majorDiagonal: false,
-      minorDiagonal: false,
-      rooks: true,
-      queens: true
-    });
+    ]);
   });
 
   it("should find column conflicts", function() {
-    checkAllConflictTypes([
+    verifyConflictTypes(['col', 'rooks', 'queens'], [
       [1, 0, 0, 0],
       [0, 0, 0, 0],
       [1, 0, 0, 0],
       [0, 0, 0, 0]
-    ], {
-      row: false,
-      col: true,
-      majorDiagonal: false,
-      minorDiagonal: false,
-      rooks: true,
-      queens: true
-    });
+    ]);
   });
 
   it("should find major diagonal conflicts", function() {
-    checkAllConflictTypes([
+    verifyConflictTypes(['majorDiagonal', 'queens'], [
       [0, 1, 0, 0],
       [0, 0, 1, 0],
       [0, 0, 0, 0],
       [0, 0, 0, 0]
-    ], {
-      row: false,
-      col: false,
-      majorDiagonal: true,
-      minorDiagonal: false,
-      rooks: false,
-      queens: true
-    });
-
+    ]);
   });
 
   it("should find minor diagonal conflicts", function() {
-    checkAllConflictTypes([
+    verifyConflictTypes(['minorDiagonal', 'queens'], [
       [0, 0, 1, 0],
       [0, 0, 0, 0],
       [1, 0, 0, 0],
       [0, 0, 0, 0]
-    ], {
-      row: false,
-      col: false,
-      majorDiagonal: false,
-      minorDiagonal: true,
-      rooks: false,
-      queens: true
-    });
+    ]);
   });
 
 });

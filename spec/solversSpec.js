4ed6523198e9b7ea3991372b7b7a9ec1d319b4a3
Marcus Phillips <github.com@marcusphillips.com>
2013-04-12 03:27:44 -0700

    tests for solvers

--- spec/solversSpec.js
+++ spec/solversSpec.js
@@ -1,3 +1,50 @@
 describe("solvers", function() {
   window.displayBoard = function(){};
+
+  describe('findNRooksSolution()', function(){
+
+    it('finds a valid solution for n of 0-8', function(){
+      _.range(1, 8).map(function(n){
+        var solutionBoard = new Board(findNRooksSolution(n));
+        expect(solutionBoard.hasAnyRooksConflicts()).toEqual(false);
+      });
+    });
+
+  });
+
+  describe('countNRooksSolutions()', function(){
+
+    it('finds the number of valid solutions for n of 0-8', function(){
+      _.range(0, 8).map(function(n){
+        var solutionCount = countNRooksSolutions(n);
+        var expectedSolutionCount = [1, 1, 2, 6, 24, 120, 720, 5040][n];
+        expect(solutionCount).toEqual(expectedSolutionCount);
+      });
+    });
+
+  });
+
+  describe('findNQueensSolution()', function(){
+
+    it('finds a valid solution for n of 0-8', function(){
+      _.range(1, 8).map(function(n){
+        var solutionBoard = new Board(findNQueensSolution(n));
+        expect(solutionBoard.hasAnyQueensConflicts()).toEqual(false);
+      });
+    });
+
+  });
+
+  describe('countNQueensSolutions()', function(){
+
+    it('finds the number of valid solutions for n of 0-8', function(){
+      _.range(0, 8).map(function(n){
+        var solutionCount = countNQueensSolutions(n);
+        var expectedSolutionCount = [1, 1, 0, 0, 2, 10, 4, 40, 92][n];
+        expect(solutionCount).toEqual(expectedSolutionCount);
+      });
+    });
+
+  });
+
 });
